e7063aefd6ef2295eb007bd29e588c571abaaf27
verovede <veroferrariback@gmail.com>
2022-09-10 23:49:50 -0300

    CRUD USUARIOS

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <Header />
+ <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./componentes/Header.vue";
+import Footer from "./componentes/Footer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+
+    Footer,
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+.container-fluid {
+  padding-right: 0px !important ;
+  padding-left: 0px !important;
+}
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1050;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  outline: 0;
+}
+
+.bg-tag {
+  background: darkcyan;
+}
+
+.cajaCurso {
+  background: black;
+  min-height: 400px;
+  color: white;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+//import Router
+import { router } from './router'
+
+//import estilos
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+
+
+/*GLOBAL FILTERS */
+import './globalFilters'
+
+//import form
+import './form'
+
+//import AXIOS
+import './axios'
+
+//import VUEX
+import store from './store'
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
+
